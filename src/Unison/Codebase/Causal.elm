c6ab7b4b05e2f8f19ba964ef2c523e50cf7c51bf
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-08-16 19:03:56 -0400

    Unison.Codebase.Causal

--- src/Unison/Codebase/Causal.elm
+++ src/Unison/Codebase/Causal.elm
@@ -1,5 +1,13 @@
 module Unison.Codebase.Causal exposing (..)
 
+import Set exposing (Set)
+import Unison.Codebase.Branch exposing (..)
+import Unison.Hash exposing (Hash)
 
+
+{-| Haskell type: Unison.Codebase.Causal.Raw
+-}
 type Causal
-    = Causal
+    = RawOne Branch
+    | RawCons Branch Hash
+    | RawMerge Branch (Set Hash)

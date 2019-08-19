0a7c1b4441201a32a36863f78d2e2640aed51af9
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-08-18 23:15:54 -0400

    Pattern type

--- src/Unison/Pattern.elm
+++ src/Unison/Pattern.elm
@@ -1,10 +1,31 @@
 module Unison.Pattern exposing (..)
 
+import Int64 exposing (Int64)
+import Unison.Reference exposing (Reference)
+import Word64 exposing (Word64)
 
+
+{-| Haskell type: Unison.Pattern.Pattern
+-}
 type Pattern ann
-    = Undefined1 ann
+    = UnboundP ann
+    | VarP ann
+    | BooleanP ann Bool
+    | IntP ann Int64
+    | NatP ann Word64
+    | FloatP ann Float
+    | TextP ann String
+    | CharP ann Char
+    | ConstructorP ann Reference Int (List (Pattern ann))
+    | AsP ann (Pattern ann)
+    | EffectPureP ann (Pattern ann)
+    | EffectBindP ann Reference Int (List (Pattern ann)) (Pattern ann)
+    | SequenceLiteralP ann (List (Pattern ann))
+    | SequenceOpP ann (Pattern ann) SeqOp (Pattern ann)
 
 
+{-| Haskell type: Unison.Pattern.SeqOp
+-}
 type SeqOp
     = Cons
     | Snoc
